b84f8b1419391161b1a8951e72f0f0e8c56d0724
Chad Altenberger <chadaltenberger@gmail.com>
2020-11-24 19:32:09 -0500

    Starting the header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import Home from './components/Home';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <Home />
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
 
 function Header() {
   return (
     <div className='header'>
-      
+      <Link className='header__brand' to='/'>
+        <h1 className='header__brand--logo'>HDTN</h1>
+      </Link>
+      <nav className='header__nav' role='navigation'>
+        <span>Listen</span>
+        <span>Read</span>
+        <span>Credits</span>
+        <span>Donate</span>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Header className='header'
+export default Header;
