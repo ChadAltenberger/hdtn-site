b63de6025549ec2f3003c08b6a932b3333445b2b
Chad Altenberger <chadaltenberger@gmail.com>
2020-11-24 21:12:30 -0500

    Starting to fill in the song data in songs.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,16 @@ function Header() {
   return (
     <div className='header'>
       <Link className='header__brand' to='/'>
-        <h1 className='header__brand--logo'>HDTN</h1>
+        <h2 className='header__brand--logo'>HDTN</h2>
       </Link>
       <nav className='header__nav' role='navigation'>
-        <span>Listen</span>
-        <span>Read</span>
-        <span>Credits</span>
-        <span>Donate</span>
+        <ul>
+          <li className='header__nav--item'>Home</li>
+          <li className='header__nav--item'>Listen</li>
+          <li className='header__nav--item'>Read</li>
+          <li className='header__nav--item'>Credits</li>
+          <li className='header__nav--item'>Donate</li>
+        </ul>
       </nav>
     </div>
   );
---
+++ src/songs.js
@@ -0,0 +1,87 @@
+export const SONGS = [
+  {
+    id: 0,
+    title: 'Prologue',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 1,
+    title: 'Head In The Clouds',
+    image: '',
+    lyrics:
+      "I think there's still a lack of relationship,\nCause who You are is not who I could be,\nAnd if I seem a little bitter then you better put my feet down, down\nCause I got my head in the clouds, head in the clouds,\nI'm in the wind, not the rain.\nI got my head in the clouds, head in the clouds,\nJust let me sing, sing again.\nI feel a little forced to rhyme sometimes,\nFeel a little forced to smile sometimes,\nFeel like I could never express this right,\nLike everything I sing is wrong, so why try?\nIt just isn't like me to like me,\nAnd I get the feeling that You like to fight me.\nIf what I'm saying here is highly unlikely,\nThen open up the door I shut, tonight, and come find me.\n(So runaway, take the money, and sight see,\nI get the feeling that your feelings would like me.)\nThis opportunity just sounds like it might be,\nThe perfect thing for me, cause all my Father wants to do is keep,\nTaking everything away from me.\nYou said Your promise was peace!\nBut You seem more like a fighter!\nYou said Your burden was light!\nBut I don't feel any lighter! (X2)\nI question why You never take the blame for this relationship,\nCause who I am is not who I should be.\n(You seem to think a little better when you're bitter from the beat down, down.)\nCould somebody help me? I gotta get away now.\nSomebody tell me, if I'll ever feel alive, how\nBitter I've become because you keep letting my mind sink down.\n(drown drown)\nGod You got Your head in the clouds, head in the clouds,\nYou send the wind, not the rain.\nYou got Your head in the clouds, head in the clouds,\nJust let me sing, sing again.\nI think there's still a lack in this relationship,\nCause who I am is not the man I ever wanted to be.\nAnd I don't need a compliment,\nYou gotta get inside my mind, cause You're the vine,\nI'm just a different limb of the poet tree.\nDo You remember the crash?\nDo You remember the last time we spoke?\nIt was the last time I felt hope.\nBut where did You go?\nI think I'm losing my smoke,\nSo mirror, mirror tell me if it's all a joke.\nCause buried deep inside,\nBuried in the back of my mind,\nWell, I've been trying to hide,\nAnd trying to lie,\nBut if You got the time,\nMaybe You can try to pick apart the cynical hymns of men who stand alone in line.\nJust look me in the eyes.\nTell me why do good men die?\nOpen Your mouth and speak to me tonight.\nWhat do you mean it seems our history has seen that only He can qualify?\nWhy am I so terrified to change particular cadences?\nTo make some different acquaintances?\nIs this a test? Is this Your best?\nI bet You think regret with every breath.\nMy chest is tightening. Your voice is frightening.\nAnd everything I thought I knew is wrong.\nYou have to get outside my mind.\nI'm leaving home tonight.\nI packed my baggage and I'm heading off into the night.\nAnd if I don't survive, well then at least I tried.\nBut this is it, so save the tears for someone else, and Tell the world I died.\nTell the world I died.\nTell the world I died.\nNo! No! I'm afraid to let You go!\nI don't wanna die.\nBut this is it, so keep Your love and tell the world that I'm alive!\nI am alive!",
+    track: ''
+  },
+  {
+    id: 2,
+    title: "You Aren't Allowed To Leave",
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 3,
+    title: 'We All Hide',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 4,
+    title: 'Give Up! The Ghosts Are Coming!',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 5,
+    title: 'Not By Force, Not By Strength',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 6,
+    title: 'Feet On The Ground',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 7,
+    title: 'Break The Teeth',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 8,
+    title: 'Even The Grave',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 9,
+    title: 'Forgive, Act, Repay',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 10,
+    title: 'Sing, Sing Again',
+    image: '',
+    lyrics: '',
+    track: ''
+  },
+  {
+    id: 11,
+    title: 'Epilogue',
+    image: '',
+    lyrics: '',
+    track: ''
+  }
+];
