cde3f1b535cfbee0df085eee73b8bdc45be83247
Chad Altenberger <chadaltenberger@gmail.com>
2021-03-06 08:42:24 -0500

    Fixed header on Read and Donate

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Backdrop from './Backdrop';
+import Header from './Header';
 import '../styles/Donate.css';
 
 function Donate() {
 	return (
 		<>
+			<Header color='rgb(44, 43, 43, 0.05)' />
 			<div className='backdrop'></div>
 			<div className='donateMain'>
 				<h1>Donate to an organization.</h1>
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Backdrop from './Backdrop';
+import Header from './Header';
 import '../styles/Read.css';
 
 function Read() {
 	return (
 		<>
+			<Header color='rgb(44, 43, 43, 0.05)' />
 			<div className='backdrop'></div>
 			<div className='readMain'>
 				<h1>Read the story that accompanies the album.</h1>
